0c55845d396deb7080a277d45bf3b287049df671
Henrylle Maia <henryllemaia@gmail.com>
2024-02-14 18:06:42 -0300

    teste de deploy

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "black"}
-          text={showAdd ? "Close" : "Add"}
+          color={showAdd ? "black" : "blue"}
+          text={showAdd ? "Close" : "Adicionar"}
           onClick={onAdd}
         />
       )}
