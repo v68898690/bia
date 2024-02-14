d4bd845c8c5ace3deb1fa47e82f7e6130d73a679
Henrylle Maia <henryllemaia@gmail.com>
2024-02-14 20:13:58 -0300

    mudando para ingles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "black" : "green"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          color={showAdd ? "red" : "blue"}
+          text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
       )}
