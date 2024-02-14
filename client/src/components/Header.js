1016558f9227933acd40f9cd2c419c8f404236dd
Henrylle Maia <henryllemaia@gmail.com>
2024-02-14 19:21:35 -0300

    mudando cor

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "black" : "blue"}
+          color={showAdd ? "black" : "green"}
           text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
